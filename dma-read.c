298e6487594b3cfb645b3934cc5f72b87822a0b5
server <fandahao17@mail.ustc.edu.cn>
2020-04-09 22:13:08 +0800

    Add more macros

--- dma-read.c
+++ dma-read.c
@@ -3,14 +3,78 @@
 
 #include "mailbox.h"
 
-#define PAGE_SIZE 4096
+#define PAGE_SIZE       4096
 
-#define PERI_BASE 0x7E000000
+#define PERI_BASE       0x7E000000
 
+#define GPIO_BASE       (PERI_BASE + 0x00200000)
+#define GPLEV0          13
+#define GPIO_LEN        0xF4
 
-#define DMA_BASE  0x00007000
-#define DMA_CHANNEL 5
-#define DMA_OFFSET  0x100
+#define PWM_BASE        (PERI_BASE + 0x0020C000)
+#define PWM_LEN         0x28
+#define CLK_PWMCTL      40
+#define CLK_PWMDIV      41
+
+/* PWM control bits */
+#define PWM_CTL      0
+#define PWM_STA      1
+#define PWM_DMAC     2
+#define PWM_RNG1     4
+#define PWM_DAT1     5
+#define PWM_FIFO     6
+#define PWM_RNG2     8
+#define PWM_DAT2     9
+
+#define PWM_CTL_MSEN2 (1<<15)
+#define PWM_CTL_PWEN2 (1<<8)
+#define PWM_CTL_MSEN1 (1<<7)
+#define PWM_CTL_CLRF1 (1<<6)
+#define PWM_CTL_USEF1 (1<<5)
+#define PWM_CTL_MODE1 (1<<1)
+#define PWM_CTL_PWEN1 (1<<0)
+
+#define PWM_DMAC_ENAB      (1 <<31)
+#define PWM_DMAC_PANIC(x) ((x)<< 8)
+#define PWM_DMAC_DREQ(x)   (x)
+
+#define SYSTIMER_BASE   (PERI_BASE + 0x3000)
+#define SYST_LEN        0x1C
+#define SYST_CLO        1
+
+#define DMA_BASE        (PERI_BASE + 0x00007000)
+#define DMA_CHANNEL     5
+#define DMA_OFFSET      0x100
+#define DMA_ADDR        (DMA_BASE + DMA_OFFSET * DMA_CHANNEL)
+
+/* DMA CS Control and Status bits */
+#define DMA_ENABLE (0xFF0/4)
+#define DMA_CHANNEL_RESET       (1<<31)
+#define DMA_CHANNEL_ABORT       (1<<30)
+#define DMA_WAIT_ON_WRITES      (1<<28)
+#define DMA_PANIC_PRIORITY(x) ((x)<<20)
+#define DMA_PRIORITY(x)       ((x)<<16)
+#define DMA_INTERRUPT_STATUS    (1<< 2)
+#define DMA_END_FLAG            (1<< 1)
+#define DMA_ACTIVE              (1<< 0)
+
+/* DMA control block "info" field bits */
+#define DMA_NO_WIDE_BURSTS          (1<<26)
+#define DMA_PERIPHERAL_MAPPING(x) ((x)<<16)
+#define DMA_BURST_LENGTH(x)       ((x)<<12)
+#define DMA_SRC_IGNORE              (1<<11)
+#define DMA_SRC_DREQ                (1<<10)
+#define DMA_SRC_WIDTH               (1<< 9)
+#define DMA_SRC_INC                 (1<< 8)
+#define DMA_DEST_IGNORE             (1<< 7)
+#define DMA_DEST_DREQ               (1<< 6)
+#define DMA_DEST_WIDTH              (1<< 5)
+#define DMA_DEST_INC                (1<< 4)
+#define DMA_WAIT_RESP               (1<< 3)
+
+#define NORMAL_DMA (DMA_NO_WIDE_BURSTS | DMA_WAIT_RESP)
+
+#define TIMED_DMA(x) (DMA_DEST_DREQ | DMA_PERIPHERAL_MAPPING(x))
 
 #define TICKS_PER_PAGE  20
 #define LEVELS_PER_PAGE 1000
